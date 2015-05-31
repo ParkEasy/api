cf4e33b5d7e69cde828896a5cc85c6f4f630639f
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-05-31 21:30:09 +0200

    Fix distanzberechnung

--- Models/CoordinateModel.cs
+++ Models/CoordinateModel.cs
@@ -9,7 +9,10 @@ namespace ParkEasyAPI.Models
 		
 		public double DistanceTo(CoordinateModel coordinate)
 		{
-			double dst = Math.Sqrt(Math.Pow(coordinate.Latitude - this.Longitude, 2) + Math.Pow(coordinate.Longitude - this.Latitude, 2));
+			double dst = Math.Sqrt(
+				Math.Pow(coordinate.Latitude - this.Latitude, 2) + 
+				Math.Pow(coordinate.Longitude - this.Longitude, 2)
+			);
 			return Math.Abs(dst);
 		}
 	}
