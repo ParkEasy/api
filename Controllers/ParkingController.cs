4a8d93e79b0ca6af04c8f5b0115e589b88445af7
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-06-07 12:43:31 +0200

    Maximum Parking hours eingerechnet

--- Controllers/ParkingController.cs
+++ Controllers/ParkingController.cs
@@ -25,29 +25,32 @@ namespace ParkEasyAPI.Controllers
             
             // GARAGE DATA //
             
+            // use cache or fetch remote?
             dynamic jsonGarage;
-            
-            // use cache
             if(Cache.GarageData != null && Cache.GarageDataExpiration.HasValue && Cache.GarageDataExpiration.Value.CompareTo(DateTime.Now) >= 0) 
             {
+                // load data from cache
                 Console.WriteLine("Garages from Cache");
                 jsonGarage = Cache.GarageData;
             }
             else 
             {
+                // load data from remote source
                 Console.WriteLine("Garages from WWW");
                 using(var client = new System.Net.WebClient())
                 {
                     var body = client.DownloadString("http://www.stadt-koeln.de/externe-dienste/open-data/parking.php");
-                    jsonGarage = JsonConvert.DeserializeObject(body); 
+                    jsonGarage = JsonConvert.DeserializeObject(body);
+                    
+                    // put data into cache 
                     Cache.GarageData = jsonGarage;
                     Cache.GarageDataExpiration = DateTime.Now.AddMinutes(5);
                 }
             }
             
             // parse parking garages into our parking model
-            foreach(dynamic garage in jsonGarage.features) {
-                
+            foreach(dynamic garage in jsonGarage.features) 
+            {    
                 ParkingModel model = new ParkingModel();
                 
                 // parse garage data
@@ -64,26 +67,28 @@ namespace ParkEasyAPI.Controllers
                 model.Coordinate = coordinateModel;
                 
                 model.DistanceToUser = model.Coordinate.DistanceTo(currentPosition);
-                
             }
             
             // MACHINE DATA //
             
+            // use cache or fetch remote?
             dynamic jsonMachine;
-            
-            // use cache
             if(Cache.MachineData != null && Cache.MachineDataExpiration.HasValue && Cache.MachineDataExpiration.Value.CompareTo(DateTime.Now) >= 0) 
             {
+                // load data from cache
                 Console.WriteLine("Machine from Cache");
                 jsonMachine = Cache.MachineData;
             }
             else 
             {
+                // load data from remote source
                 Console.WriteLine("Machine from WWW");
                 using(var client = new System.Net.WebClient())
                 {
                     var body = client.DownloadString("http://geoportal1.stadt-koeln.de/ArcGIS/rest/services/66/Parkscheinautomaten/MapServer/0/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=id%20is%20not%20null&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=%2A&f=json");
                     jsonMachine = JsonConvert.DeserializeObject(body); 
+                    
+                    // put data in cache for 5 minutes
                     Cache.MachineData = jsonMachine;
                     Cache.MachineDataExpiration = DateTime.Now.AddMinutes(5);
                 }
@@ -119,26 +124,30 @@ namespace ParkEasyAPI.Controllers
             
             // UNI DATA //
             
+            // use cache or fetch from remote?
             dynamic jsonUni;
-            
-            // use cache
             if(Cache.UniData != null && Cache.UniDataExpiration.HasValue && Cache.UniDataExpiration.Value.CompareTo(DateTime.Now) >= 0) 
             {
+                // load data from cache
                 Console.WriteLine("Uni from Cache");
                 jsonUni = Cache.UniData;
             }
             else 
             {
+                // load data from remote source
                 Console.WriteLine("Uni from WWW");
                 using(var client = new System.Net.WebClient())
                 {
                     var body = client.DownloadString("https://raw.githubusercontent.com/ParkEasy/tools/master/uni.json");
                     jsonUni = JsonConvert.DeserializeObject(body); 
+                    
+                    // put data in cache for 5 minutes
                     Cache.UniData = jsonUni;
                     Cache.UniDataExpiration = DateTime.Now.AddMinutes(5);
                 }
             }
             
+            // loop university parking spaces
             foreach(dynamic uniparking in jsonUni) 
             {
                 ParkingModel model = new ParkingModel();
@@ -154,15 +163,19 @@ namespace ParkEasyAPI.Controllers
                 // parse opening hours
                 if(DynamicExist(uniparking, "hours")) 
                 {
+                    model.OpeningHours = new OpeningHoursModel[7];
+                    
                     int i = 0;
                     foreach(dynamic hour in uniparking.hours)
                     {
                         OpeningHoursModel hoursModel = new OpeningHoursModel();
-                        if(hour != null) {
+                        if(hour != null) 
+                        {
                             hoursModel.Open = hour.open;
                             hoursModel.Close = hour.close;
                         }
-                        else {
+                        else 
+                        {
                             hoursModel.Closed = true;
                         }
                         
@@ -209,10 +222,42 @@ namespace ParkEasyAPI.Controllers
             parkingModels = parkingModels.Where(delegate(ParkingModel a)
             {
                 return a.DistanceToUser < radius / 1000;
+                
             }).ToList<ParkingModel>();
             
-            // apply scoring model
-            // ...
+            // filter out places that have maximum parking hours lower than needed or 
+            // opening hours that exceed the amount of time the user wants to park
+            parkingModels = parkingModels.Where(delegate(ParkingModel a)
+            {
+                // are there limitations on the amount of parking hours?
+                if(a.MaximumParkingHours.HasValue) 
+                {
+                    // does this exceed the amount of hours the user wants to park?
+                    if(a.MaximumParkingHours.Value > hours)
+                    {
+                        return false;
+                    }
+                }
+                
+                // are there opening hours?
+                if(a.OpeningHours != null)
+                {
+                    // find opening hours of current day
+                    int day = (int) DateTime.Now.DayOfWeek;
+                    OpeningHoursModel openingModel = a.OpeningHours[day];
+                    
+                    // is the parking closed today?
+                    if(openingModel.Closed == true) 
+                    {
+                        return false;
+                    }
+                    
+                    // TODO: check if current time + parking duration in limits of opening hours
+                }
+                
+                return true;
+                
+            }).ToList<ParkingModel>();
             
             // sort by closeness to current position
             parkingModels.Sort(delegate(ParkingModel a, ParkingModel b)
