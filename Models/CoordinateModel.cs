ac1bad05388655e440c7777addc10099b4570bed
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-06-27 01:05:41 +0200

    umbau auf manhattan distance

--- Models/CoordinateModel.cs
+++ Models/CoordinateModel.cs
@@ -11,10 +11,14 @@ namespace ParkEasyAPI.Models
 		// Calculates the euclidian distance between two coordinates in meters
 		public double DistanceTo(CoordinateModel coordinate)
 		{
-			double dst = Math.Sqrt(
+			// euclidean distance
+			/*double dst = Math.Sqrt(
 				Math.Pow(this.Latitude - coordinate.Latitude, 2) + 
 				Math.Pow(this.Longitude - coordinate.Longitude, 2)
-			);
+			);*/
+			
+			// manhattan distance
+			double dist = Math.Abs(this.Latitude - coordinate.Latitude) + Math.Abs(this.Longitude - coordinate.Longitude);
 			return Math.Abs(dst) * 6371 / 1000;
 		}
 	}
